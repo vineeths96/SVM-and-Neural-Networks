45a4437a3a317b08658b0e17e2eb9da1dfebca64
vineeths96 <vinsree1996@gmail.com>
2020-05-30 11:32:11 +0530

    Update Problem 1

--- problem_1/problem_1_DNN.py
+++ problem_1/problem_1_DNN.py
@@ -1,45 +1,48 @@
-import os
-os.environ["CUDA_VISIBLE_DEVICES"]="-1"
-
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.optimizers import RMSprop
-from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping
 from problem_1.load_data import load_data
 from problem_1.parameters import *
+
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 
 # DNN Classifier
 def problem_1_DNN():
-    # Load data
-    X_train, Y_train, X_test, Y_test = load_data('/2class-Synthetic/data_noise_0.txt')
+    os.remove('./results/problem_1_DNN.txt')
 
-    # Define model
-    model = Sequential()
-    model.add(Input(shape=(DIMENSIONS)))
-    model.add(Dense(units=64, activation='relu'))
-    model.add(Dense(units=128, activation='relu'))
-    model.add(Dense(units=32, activation='relu'))
-    model.add(Dense(units=NUM_CLASS, activation='softmax'))
+    noise_level = [0, 20, 40]
+    for noise in noise_level:
+        # Load data
+        X_train, Y_train, X_test, Y_test = load_data(f'/2class-Synthetic/data_noise_{noise}.txt')
 
-    # Define optimizer
-    optimizer = RMSprop(learning_rate=LEARNING_RATE)
-    model.compile(optimizer=optimizer,
-                  loss='sparse_categorical_crossentropy',
-                  metrics=['accuracy'])
+        # Define model
+        model = Sequential()
+        model.add(Input(shape=DIMENSIONS))
+        model.add(Dense(units=64, activation='relu'))
+        model.add(Dense(units=128, activation='relu'))
+        model.add(Dense(units=32, activation='relu'))
+        model.add(Dense(units=NUM_CLASS, activation='softmax'))
 
-    # Train the model
-    history = model.fit(x=X_train,
-                        y=Y_train,
-                        epochs=EPOCHS,
-                        batch_size=BATCH_SIZE,
-                        validation_split=0.1,
-                        verbose=0)
+        # Define optimizer
+        optimizer = RMSprop(learning_rate=LEARNING_RATE)
+        model.compile(optimizer=optimizer,
+                      loss='sparse_categorical_crossentropy',
+                      metrics=['accuracy'])
 
-    # Evaluate the model
-    loss, accuracy = model.evaluate(x=X_test, y=Y_test)
+        # Train the model
+        history = model.fit(x=X_train,
+                            y=Y_train,
+                            epochs=EPOCHS,
+                            batch_size=BATCH_SIZE,
+                            validation_split=0.1,
+                            verbose=0)
 
-    print(f"The DNN classification accuracy is: {accuracy * 100:0.2f}%")
-    with open('./results/problem_1_DNN.txt', "w") as file:
-        file.write(f"The DNN classification accuracy is: {accuracy * 100:0.2f}%\n")
+        # Evaluate the model
+        loss, accuracy = model.evaluate(x=X_test, y=Y_test)
+
+        print(f"The DNN classification accuracy {noise}% label noise is: {accuracy * 100:0.2f}%")
+        with open('./results/problem_1_DNN.txt', "a") as file:
+            file.write(f"The DNN classification accuracy {noise}% label noise is: {accuracy * 100:0.2f}%\n")
--- problem_1/problem_1_SVM.py
+++ problem_1/problem_1_SVM.py
@@ -1,3 +1,4 @@
+import os
 from sklearn.svm import SVC
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
@@ -6,23 +7,27 @@ from problem_1.load_data import load_data
 
 # SVM Classifier
 def problem_1_SVM():
-    # Load data
-    X_train, Y_train, X_test, Y_test = load_data('/2class-Synthetic/data_noise_0.txt')
+    os.remove('./results/problem_1_SVM.txt')
 
-    # Gaussian Kernel
-    gauss_clf = make_pipeline(StandardScaler(), SVC(kernel='rbf', gamma='scale'))
-    gauss_clf.fit(X_train, Y_train)
-    gauss_accuracy = gauss_clf.score(X_test, Y_test)
+    noise_level = [0, 20, 40]
+    for noise in noise_level:
+        # Load data
+        X_train, Y_train, X_test, Y_test = load_data(f'/2class-Synthetic/data_noise_{noise}.txt')
 
-    print(f"The SVM classification accuracy with Gaussian Kernel is: {gauss_accuracy * 100:0.2f}%")
-    with open('./results/problem_1_SVM.txt', "w") as file:
-        file.write(f"The SVM classification accuracy with Gaussian Kernel is: {gauss_accuracy * 100:0.2f}%\n")
+        # Gaussian Kernel
+        gauss_clf = make_pipeline(StandardScaler(), SVC(kernel='rbf', gamma='scale'))
+        gauss_clf.fit(X_train, Y_train)
+        gauss_accuracy = gauss_clf.score(X_test, Y_test)
 
-    # Polynomial Kernel
-    poly_clf = make_pipeline(StandardScaler(), SVC(kernel='poly', gamma='scale'))
-    poly_clf.fit(X_train, Y_train)
-    poly_accuracy = poly_clf.score(X_test, Y_test)
+        print(f"The SVM classification accuracy with Gaussian Kernel with {noise}% label noise is: {gauss_accuracy * 100:0.2f}%")
+        with open('./results/problem_1_SVM.txt', "a") as file:
+            file.write(f"The SVM classification accuracy with Gaussian Kernel with {noise}% label noise is is: {gauss_accuracy * 100:0.2f}%\n")
 
-    print(f"The SVM classification accuracy with Polynomial Kernel is: {poly_accuracy * 100:0.2f}%")
-    with open('./results/problem_1_SVM.txt', "a") as file:
-        file.write(f"The SVM classification accuracy with Polynomial Kernel is: {poly_accuracy * 100:0.2f}%\n")
+        # Polynomial Kernel
+        poly_clf = make_pipeline(StandardScaler(), SVC(kernel='poly', gamma='scale'))
+        poly_clf.fit(X_train, Y_train)
+        poly_accuracy = poly_clf.score(X_test, Y_test)
+
+        print(f"The SVM classification accuracy with Polynomial Kernel with {noise}% label noise is is: {poly_accuracy * 100:0.2f}%")
+        with open('./results/problem_1_SVM.txt', "a") as file:
+            file.write(f"The SVM classification accuracy with Polynomial Kernel with {noise}% label noise is is: {poly_accuracy * 100:0.2f}%\n")
